5f844a695d80ed3e94e5dc02be4aa92f06c5331f
maupacheco <maupacheco301@gmail.com>
2020-06-09 23:48:18 -0500

    T1

---
+++ main.c
@@ -0,0 +1,43 @@
+# include < stdio.h >   
+//PACHECO SALGADO MAURICIO
+//GRUPO: 13
+// TAREA 1
+
+
+// "Escriba las líneas de C para generar un tipo complejo"
+typedef  struct _complex complex;
+struct _complex
+{
+    int a;
+    int b;
+};
+// "Escriba las líneas de C para un tipo Alumno: nombre, no_cuenta, semestre, carrera"
+struct Alumno
+{
+    char nombre [ 50 ];
+    int no_cuenta;
+    int semestre;
+    char carrera [ 20 ];
+};
+// "Escriba las líneas de C para un tipo de dato nodo que apunta al siguiente"
+typedef  struct _node node;
+struct _node {
+    int d;
+    nodo * siguiente;
+};
+// "Escriba una estructura en c generar una matriz de apuntadores"
+struct matrixap
+{
+    int * ap [ 4 ] [ 5 ];
+};
+// "Escriba el código en C para generar un arreglo entero de apuntadores
+structura princi 
+{
+    int * ap [ 5 ];
+};
+int  main ()
+{
+    // "Escriba el código en C para indicar un apuntador a nodo"
+    // NO PUDE 
+    
+}
